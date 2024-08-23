5dbd5f492d31df9b68d741833d91f65084272a43
cutekitten000 <danilo.aguiar.dev@gmail.com>
2024-08-23 15:41:05 -0300

    Create User.java

---
+++ studying/downcasting/gerenciamento_usuarios_permissoes/User.java
@@ -0,0 +1,25 @@
+class User {
+    private String username;
+    private String password;
+    
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+    
+    public String getUsername() {
+        return username;
+    }
+    
+    public String getPassword() {
+        return password;
+    }
+    
+    public boolean authenticate(String password) {
+        return this.password.equals(password);
+    }
+    
+    public void printUserDetails() {
+        System.out.println("Username: " + getUsername());
+    }
+}
