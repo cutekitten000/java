fac114d48da5c328cfebe7e84ebb35a39d1bce15
cutekitten000 <danilo.aguiar.dev@gmail.com>
2024-09-12 13:34:56 -0300

    Create Circle.java

---
+++ studying/interface/formas_geometricas/Circle.java
@@ -0,0 +1,22 @@
+public class Circle implements GeometricShape {
+    private double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    @Override
+    public double calculateArea() {
+        return Math.PI * Math.pow(radius, 2);
+    }
+
+    @Override
+    public double calculatePerimeter() {
+        return 2 * Math.PI * radius;
+    }
+
+}
