396d6e5d85dc180b7e5b824ad586d4d12a7966c0
cutekitten000 <danilo.aguiar.dev@gmail.com>
2024-09-12 13:34:39 -0300

    Create Rectangle.java

---
+++ studying/interface/formas_geometricas/Rectangle.java
@@ -0,0 +1,27 @@
+public class Rectangle implements GeometricShape {
+    private double width;
+    private double height;
+
+    public Rectangle(double width, double height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    @Override
+    public double calculateArea() {
+        return width * height;
+    }
+
+    @Override
+    public double calculatePerimeter() {
+        return 2 * (width + height);
+    }
+}
