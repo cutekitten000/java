35264da13e5024f8c8cdc90d47fdca1d5b8f07c9
cutekitten000 <danilo.aguiar.dev@gmail.com>
2024-09-17 10:52:02 -0300

    Update ContractService.java

--- curso_java_nelio_alves/interfaces/exercicio_fixação/model/services/ContractService.java
+++ curso_java_nelio_alves/interfaces/exercicio_fixação/model/services/ContractService.java
@@ -1,26 +1,28 @@
-package model.services;
-
-import model.entities.Contract;
-import model.entities.Installment;
+package services;
 
 import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
+import java.util.Calendar;
 import java.util.Date;
 
+import entities.Contract;
+import entities.Installment;
+
 public class ContractService {
-    public void processContract(Contract contract, Integer months) {
-        OnlinePaymentService service = new PaypalService();
 
-
-        DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-        System.out.println("Parcelas:");
-        for (int i = 0; i < months; i++) {
-            double amount = service.interest(contract.getTotalValue() / months, i + 1);
-            LocalDate installmentDate = contract.getDate().plusMonths(i);
-
-            String formattedDate = installmentDate.format(fmt);
-
-            System.out.println(formattedDate + " - " + String.format("%.2f", service.paymentFee(amount)));
+	private OnlinePaymentService onlinePaymentService;
+	
+	public ContractService(OnlinePaymentService onlinePaymentService) {
+		this.onlinePaymentService = onlinePaymentService;
+	}
+	
+	public void processContract(Contract contract, int months) {
+		double basicQuota = contract.getTotalValue() / months;
+        for (int i = 1; i <= months; i++) {
+            LocalDate dueDate = contract.getDate().plusMonths(i);
+            double interest = onlinePaymentService.interest(basicQuota, i);
+            double fee = onlinePaymentService.paymentFee(basicQuota + interest);
+            double quota = basicQuota + interest + fee;
+            contract.getInstallments().add(new Installment(dueDate, quota));
         }
-    }
+	}
 }
